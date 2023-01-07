6290fbc32dfd7e0556b65d45af03f08d462cd469
DooHwanKim0419 <dkimh0419@gmail.com>
2023-01-06 21:38:24 -0500

    Solved Maximum Depth of Binary Tree

---
+++ src/easy/maxDepth/Solution.java
@@ -0,0 +1,13 @@
+package easy.maxDepth;
+
+import util.TreeNode;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
